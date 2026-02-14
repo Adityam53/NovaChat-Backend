2860480f95fccd25f376c8f8cc280ea9340018a2
Aditya Moorjmalani <adityamoorjmalani@Adityas-MacBook-Air.local>
2026-02-15 00:45:30 +0530

    first commit

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://nova-chat-backend.vercel.app"],
+    methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
